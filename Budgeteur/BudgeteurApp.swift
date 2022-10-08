94c7e2841e4eedae4506b7d90c9c7eb0cc4cf38c
Anthony Dickson <dican732@student.otago.ac.nz>
2022-10-09 10:16:35 +1300

    Initial Commit

---
+++ Budgeteur/BudgeteurApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgeteurApp.swift
+//  Budgeteur
+//
+//  Created by Anthony Dickson on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BudgeteurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
