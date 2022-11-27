2a0a9673fd952071fc6a4df3734d5c9293e189db
Anthony Dickson <dican732@student.otago.ac.nz>
2022-11-27 18:44:16 +1300

    Remove unused code from `RecurrencePeriod`

--- Budgeteur/Model/RecurrencePeriod.swift
+++ Budgeteur/Model/RecurrencePeriod.swift
@@ -16,38 +16,4 @@ public enum RecurrencePeriod: String, CaseIterable {
     case monthly = "Monthly"
     case quarterly = "Quarterly"
     case yearly = "Yearly"
-    
-    /// Get the next repeat period.
-    ///
-    /// Will cycle back to the start if called on the last item.
-    func getNext() -> RecurrencePeriod {
-        let cases = Self.allCases
-        let index = cases.firstIndex(of: self)!
-        let nextIndex = (index + 1) % cases.count
-        
-        return cases[nextIndex]
-    }
-    
-    /// Convert a ``RecurrencePeriod`` to a corresponding `DateComponents` object.
-    ///
-    /// For example, `RecurrencePeriod.daily` converts to `DateComponents(day: 1)`
-    /// - Returns: A `DateComponents` objects.
-    func getDateComponents() -> DateComponents {
-        switch(self) {
-        case .never:
-            return DateComponents()
-        case .daily:
-            return DateComponents(day: 1)
-        case .weekly:
-            return DateComponents(day: 7)
-        case .fortnighly:
-            return DateComponents(day: 14)
-        case .monthly:
-            return DateComponents(month: 1)
-        case .quarterly:
-            return DateComponents(month: 3)
-        case .yearly:
-            return DateComponents(year: 1)
-        }
-    }
 }
