02f10240a100066942d10156f20ff329cd26bb7a
Anthony Dickson <dican732@student.otago.ac.nz>
2023-01-31 20:16:13 +1300

    Add watch app template

---
+++ Budgeteur Watch App/BudgeteurWatchApp.swift
@@ -0,0 +1,20 @@
+//
+//  BudgeteurWatchApp.swift
+//  BudgeteurWatch Watch App
+//
+//  Created by Anthony Dickson on 31/01/23.
+//
+
+import SwiftUI
+
+// TODO: View that shows budget summary similar to that of the iOS widget.
+// TODO: View for creating new transaction. May be best to display as a sequence of views. Should include the basics: amount, income/expense, tag and description.
+
+@main
+struct Budgeteur_Watch_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
